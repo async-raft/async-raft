321bbf6814cda0d620bd477a675cd94a4ffe7f55
Attila Bagossy <bagossyattila@outlook.com>
2021-05-04 19:05:27 +0200

    Updated the compaction test to use the new API

--- async-raft/tests/compaction.rs
+++ async-raft/tests/compaction.rs
@@ -11,13 +11,18 @@ use tokio::time::sleep;
 
 use fixtures::RaftRouter;
 
+const ENTRIES_BETWEEN_SNAPSHOTS_LIMIT: u64 = 500;
+const ORIGINAL_LEADER: u64 = 0;
+const ADDED_FOLLOWER: u64 = 1;
+const CLIENT_ID: &str = "client";
+
 /// Compaction test.
 ///
-/// What does this test do?
+/// Test plan:
 ///
-/// - build a stable single node cluster.
-/// - send enough requests to the node that log compaction will be triggered.
-/// - add new nodes and assert that they receive the snapshot.
+///   1. Create a single-node cluster of Node 0.
+///   1. Send enough requests to the node that log compaction will be triggered.
+///   1. Add a new node (Node 0), and assert that it received the snapshot.
 ///
 /// RUST_LOG=async_raft,memstore,compaction=trace cargo test -p async-raft --test compaction
 #[tokio::test(flavor = "multi_thread", worker_threads = 4)]
@@ -27,61 +32,100 @@ async fn compaction() -> Result<()> {
     // Setup test dependencies.
     let config = Arc::new(
         Config::build("test".into())
-            .snapshot_policy(SnapshotPolicy::LogsSinceLast(500))
+            .snapshot_policy(SnapshotPolicy::LogsSinceLast(ENTRIES_BETWEEN_SNAPSHOTS_LIMIT))
             .validate()
             .expect("failed to build Raft config"),
     );
     let router = Arc::new(RaftRouter::new(config.clone()));
-    router.new_raft_node(0).await;
+    router.new_raft_node(ORIGINAL_LEADER).await;
 
     // Assert all nodes are in non-voter state & have no entries.
-    sleep(Duration::from_secs(10)).await;
+    sleep(Duration::from_secs(1)).await;
     router.assert_pristine_cluster().await;
 
     // Initialize the cluster, then assert that a stable cluster was formed & held.
-    tracing::info!("--- initializing cluster");
-    router.initialize_from_single_node(0).await?;
-    sleep(Duration::from_secs(10)).await;
+    tracing::info!("--- Initializing cluster");
+    router.initialize_from_single_node(ORIGINAL_LEADER).await?;
+    sleep(Duration::from_secs(1)).await;
     router.assert_stable_cluster(Some(1), Some(1)).await;
 
     // Send enough requests to the cluster that compaction on the node should be triggered.
-    router.client_request_many(0, "0", 499).await; // Puts us exactly at the configured snapshot policy threshold.
-    sleep(Duration::from_secs(5)).await; // Wait to ensure there is enough time for a snapshot to be built (this is way more than enough).
-    router.assert_stable_cluster(Some(1), Some(500)).await;
-    router
-        .assert_storage_state(
-            1,
-            500,
-            Some(0),
-            500,
-            Some((
-                500.into(),
-                1,
-                MembershipConfig {
-                    members: hashset![0],
-                },
-            )),
-        )
-        .await;
+    // On pristine startup, we always put a single entry into the log. Thus, adding
+    // LIMIT - 1 entries more to the log exactly triggers compaction.
+    router.client_request_many(
+        ORIGINAL_LEADER,
+        CLIENT_ID,
+        (ENTRIES_BETWEEN_SNAPSHOTS_LIMIT - 1) as usize
+    ).await;
+    // Wait to ensure there is enough time for a snapshot to be built.
+    sleep(Duration::from_secs(1)).await;
 
-    // Add a new node and assert that it received the same snapshot.
-    router.new_raft_node(1).await;
-    router.add_non_voter(0, 1).await.expect("failed to add new node as non-voter");
-    /*router
-        .change_membership(0, hashset![0, 1])
-        .await
-        .expect("failed to modify cluster membership");*/
-    sleep(Duration::from_secs(5)).await; // Wait to ensure metrics are updated (this is way more than enough).
-    router.assert_stable_cluster(Some(1), Some(502)).await; // We expect index to be 500 + 2 (joint & uniform config change entries).
-    let expected_snap = Some((
-        500.into(),
+    // Assert on the snapshot made.
+    router.assert_stable_cluster(Some(1), Some(ENTRIES_BETWEEN_SNAPSHOTS_LIMIT)).await;
+    router.assert_storage_state(
         1,
-        MembershipConfig {
-            members: hashset![0u64],
-        },
-    ));
-    router.assert_storage_state(1, 502, None, 500, expected_snap).await;
-    // -------------------------------- ^^^^ this value is None because non-voters do not vote.
+        ENTRIES_BETWEEN_SNAPSHOTS_LIMIT,
+        Some(0),
+        ENTRIES_BETWEEN_SNAPSHOTS_LIMIT,
+        Some((
+            ENTRIES_BETWEEN_SNAPSHOTS_LIMIT.into(),
+            1,
+            MembershipConfig {
+                members: hashset![0],
+            },
+        )),
+    )
+    .await;
+
+    // Add a new node to the cluster.
+    {
+        tracing::info!("--- Adding new node to the cluster");
+        router.new_raft_node(ADDED_FOLLOWER).await;
+        router.add_non_voter(ORIGINAL_LEADER, ADDED_FOLLOWER).await.expect("failed to add new node as non-voter");
+        let add_voter_router = Arc::clone(&router);
+        let voter_added = tokio::spawn(async move {
+            let _ = add_voter_router.add_voter(ORIGINAL_LEADER, ADDED_FOLLOWER).await;
+        });
+
+        let request_router = Arc::clone(&router);
+        let request_processed = tokio::spawn(async move {
+            tokio::time::sleep(Duration::from_secs(1)).await;
+
+            request_router.client_request(ORIGINAL_LEADER, CLIENT_ID, 1).await;
+        });
+
+        tokio::join!(voter_added, request_processed).0?;
+    }
+
+    // Wait to ensure metrics are updated (this is way more than enough).
+    sleep(Duration::from_secs(1)).await;
+
+    // Assert that the new node received the snapshot.
+    {
+        // +2 because
+        //   1 - config change
+        //   2 - additional request
+        router.assert_stable_cluster(
+            Some(1),
+            Some(ENTRIES_BETWEEN_SNAPSHOTS_LIMIT + 2)
+        ).await;
+
+        let expected_snapshot = Some((
+            ENTRIES_BETWEEN_SNAPSHOTS_LIMIT.into(),
+            1,
+            MembershipConfig {
+                members: hashset![0u64],
+            },
+        ));
+
+        router.assert_storage_state(
+            1,
+            ENTRIES_BETWEEN_SNAPSHOTS_LIMIT + 2,
+            None, // This value is None because non-voters do not vote.
+            ENTRIES_BETWEEN_SNAPSHOTS_LIMIT + 2,
+            expected_snapshot
+        ).await;
+    }
 
     Ok(())
 }
